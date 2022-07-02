01928b757477ca807bdf0ddf34a8a8f47ddfb00b
makoto tsuyuki <mtsuyuki@gmail.com>
2022-07-02 23:22:54 +0900

    関数にして探してるものを見つけたら終わるようにした

--- http_get_parse1/main.go
+++ http_get_parse1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -9,8 +10,8 @@ import (
 	"golang.org/x/net/html"
 )
 
-func main() {
-	resp, err := http.Get("https://www.tsuyukimakoto.com/blog/2022/06/29/summer/")
+func searchOgImage(targetUrl string) (url string, err error) {
+	resp, err := http.Get(targetUrl)
 	if err != nil {
 		log.Fatalf("Error: %v\n", err)
 	}
@@ -18,15 +19,6 @@ func main() {
 
 	tokenizer := html.NewTokenizer(resp.Body)
 	for {
-		// tokenType := tokenizer.Next()
-		// if tokenType == html.ErrorToken {
-		// 	err := tokenizer.Err()
-		// 	if err == io.EOF {
-		// 		fmt.Println("Finish!")
-		// 		break
-		// 	}
-		// 	log.Fatalf("Error tokenize HTML: %v\n", tokenizer.Err())
-		// }
 
 		if tokenType := tokenizer.Next(); tokenType == html.ErrorToken {
 			if err := tokenizer.Err(); err == io.EOF {
@@ -36,24 +28,33 @@ func main() {
 			log.Fatalf("Error tokenize HTML: %v\n", tokenizer.Err())
 		}
 
-		if tagName, hasAttr := tokenizer.TagName(); hasAttr {
-			fmt.Println("Tag: ", string(tagName))
+		if tagName, hasAttr := tokenizer.TagName(); string(tagName) == "meta" && hasAttr {
+			// fmt.Println("Tag: ", string(tagName))
+			og_image_found := false
 			for {
 				key, val, hasMore := tokenizer.TagAttr()
-				fmt.Println("  Attribute: ", string(key), string(val))
+				if string(key) == "property" && string(val) == "og:image" {
+					og_image_found = true
+				}
+				if og_image_found && string(key) == "content" {
+					return string(val), nil
+				}
+				// fmt.Println("  Attribute: ", string(key), string(val))
 				if !hasMore {
 					break
 				}
 			}
 		}
 	}
+	return "", errors.New("no og:img")
+} 
 
- 		// case html.TextToken:
-		// fmt.Println("TextToken: ", tokenizer.Token().Data)
-		// case html.EndTagToken:
-		// case html.SelfClosingTagToken:
-		// case html.CommentToken:
-		// case html.DoctypeToken:
+func main() {
+	url, err := searchOgImage("https://www.tsuyukimakoto.com/blog/2022/06/29/summer/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("og:img -> ", url)
 
 // <meta property="og:image" content="https://www.tsuyukimakoto.com/static/blog/2022/0629/bridge.jpg"/>
 }
